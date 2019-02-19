1213cd8ae00a5e67b16cd6bf88b1e81ce6b1eb82
Silas Tailer <silas.tailer@gmail.com>
2019-02-19 10:43:51 -0300

    Showing an Edit Form
    Submitting answer code for module Showing an Edit Form

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -62,6 +62,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/delete":
 				  deleteBook(request, response);
 					break;
+				case "/edit":
+						showEditForm(request, response);
+						break;
         default:
 				   listBooks(request, response);
            break;
@@ -114,6 +117,13 @@ public class ControllerServlet extends HttpServlet {
 		response.sendRedirect("list");
 	}
 
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		Book editBook = bookDAO.getBook(id);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
+		request.setAttribute("book", editBook);
+		dispatcher.forward(request, response);
+	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
